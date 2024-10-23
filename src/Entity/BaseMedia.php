14d267447bcdf5ff91745c43dd1e843386d5d021
Damian Długosz <bigfootdd@gmail.com>
2024-10-23 17:58:01 +0200

    UPD

--- src/Entity/BaseMedia.php
+++ src/Entity/BaseMedia.php
@@ -13,6 +13,7 @@ declare(strict_types=1);
 
 namespace Sonata\MediaBundle\Entity;
 
+use Domain\Model\ValueObject\MediaMetadata;
 use Sonata\MediaBundle\Model\Media;
 
 abstract class BaseMedia extends Media
@@ -25,5 +26,50 @@ abstract class BaseMedia extends Media
     public function preUpdate(): void
     {
         $this->updatedAt = new \DateTimeImmutable();
+    }
+
+    public function setContext($context): void
+    {
+        //        throw new \Exception('This property is not updatable');
+    }
+
+    public function setProviderName($providerName): void
+    {
+        //        throw new \Exception('This property is not updatable');
+    }
+
+    public function getProviderName(): string
+    {
+        return 'sonata.media.provider.'.$this->providerName;
+    }
+
+    public function isImage(): bool
+    {
+        return str_starts_with($this->contentType, 'image/');
+    }
+
+    public function isVideo(): bool
+    {
+        return str_starts_with($this->contentType, 'video/');
+    }
+
+    public function getTarget(): object
+    {
+        return $this->{$this->context};
+    }
+
+    public function getPosizione(): int
+    {
+        return $this->posizione;
+    }
+
+    public function setPosizione(int $posizione): void
+    {
+        $this->posizione = $posizione;
+    }
+
+    public function getCdnIsFlushable(): bool
+    {
+        return (bool) $this->cdnIsFlushable;
     }
 }
